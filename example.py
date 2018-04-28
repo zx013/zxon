659b91e3cb7fe3d138b3ec7402e5f9d4d3427beb
zx013 <1453145267@qq.com>
2018-04-28 16:53:58 +0800

    step

--- example.py
+++ example.py
@@ -4,122 +4,160 @@ Created on Fri Apr 20 14:14:45 2018
 
 @author: zx013
 """
-from brian2 import *
+import os
+import numpy as np
 import matplotlib.pyplot as plt
+from brian2 import PoissonGroup, NeuronGroup, Synapses, StateMonitor
+from brian2 import Hz, ms, prefs, start_scope, run
 
 prefs.codegen.target = 'numpy'
 
-def cobahh():
-    area = 20000*umetre**2
-    Cm = (1*ufarad*cm**-2) * area
-    gl = (5e-5*siemens*cm**-2) * area
+class Poisson(PoissonGroup):
+    def __init__(self, h, num=1):
+        super(Poisson, self).__init__(num, h*Hz)
 
-    El = -60*mV
-    EK = -90*mV
-    ENa = 50*mV
-    g_na = (100*msiemens*cm**-2) * area
-    g_kd = (30*msiemens*cm**-2) * area
-    VT = -63*mV
+class Izhikevich(NeuronGroup):
+    __type__ = 'neuron'
 
-    taue = 5*ms
-    taui = 10*ms
+    params = \
+    ((0.02,    0.2,    -65,    6,    14), #tonic spiking
+    (0.02,    0.25,   -65,    6,    0.5), #phasic spiking
+    (0.02,    0.2,    -50,    2,    15), #tonic bursting
+    (0.02,    0.25,   -55,    0.05, 0.6), #phasic bursting
+    (0.02,    0.2,    -55,    4,    10), #mixed mode
+    (0.01,    0.2,    -65,    8,    30), #spike frequency adaptation
+    (0.02,    -0.1,   -55,    6,    0), #Class 1
+    (0.2,     0.26,   -65,    0,    0), #Class 2
+    (0.02,    0.2,    -65,    6,    7), #spike latency
+    (0.05,    0.26,   -60,    0,    0), #subthreshold oscillations
+    (0.1,     0.26,   -60,    -1,   0), #resonator
+    (0.02,    -0.1,   -55,    6,    0), #integrator
+    (0.03,    0.25,   -60,    4,    0), #rebound spike
+    (0.03,    0.25,   -52,    0,    0), #rebound burst
+    (0.03,    0.25,   -60,    4,    0), #threshold variability
+    (1,       1.5,    -60,    0,    -65), #bistability
+    (1,       0.2,    -60,    -21,  0), #DAP
+    (0.02,    1,      -55,    4,    0), #accomodation
+    (-0.02,   -1,     -60,    8,    80), #inhibition-induced spiking
+    (-0.026,  -1,     -45,    0,    80)) #inhibition-induced bursting
 
-    Ee = 0*mV
-    Ei = -80*mV
-
-    we = 6*nS
-    wi = 67*nS
-
-    eqs = Equations('''
-                    dv/dt = (gl*(El - v) + ge*(Ee - v) + gi*(Ei - v) - g_na*(m*m*m)*h*(v - ENa) - g_kd*(n*n*n*n)*(v - EK))/Cm : volt
-                    dm/dt = alpha_m*(1 - m) - beta_m*m : 1
-                    dn/dt = alpha_n*(1 - n) - beta_n*n : 1
-                    dh/dt = alpha_h*(1 - h) - beta_h*h : 1
-                    dge/dt = -ge*(1./taue) : siemens
-                    dgi/dt = -gi*(1./taui) : siemens
-                    alpha_m = 0.32*(mV**-1)*(13*mV - v + VT)/(exp((13*mV - v + VT)/(4*mV)) - 1.)/ms : Hz
-                    beta_m = 0.28*(mV**-1)*(v - VT - 40*mV)/(exp((v - VT - 40*mV)/(5*mV)) - 1)/ms : Hz
-                    alpha_n = 0.032*(mV**-1)*(15*mV - v + VT)/(exp((15*mV - v + VT)/(5*mV)) - 1.)/ms : Hz
-                    beta_n = .5*exp((10*mV - v + VT)/(40*mV))/ms : Hz
-                    alpha_h = 0.128*exp((17*mV - v + VT)/(18*mV))/ms : Hz
-                    beta_h = 4./(1 + exp((40*mV - v + VT)/(5*mV)))/ms : Hz
-                    ''')
-
-    P = NeuronGroup(4000, model=eqs, threshold='v > -20*mV', refractory=3*ms, method='exponential_euler')
-    Pe = P[:3200]
-    Pi = P[3200:]
-    Ce = Synapses(Pe, P, on_pre='ge += we')
-    Ci = Synapses(Pi, P, on_pre='gi += wi')
-    Ce.connect(p=0.02)
-    Ci.connect(p=0.02)
-
-    P.v = 'El + (randn()*5 - 5)*mV'
-    P.ge = '(randn()*1.5 + 4)*10.*nS'
-    P.gi = '(randn()*12 + 20)*10.*nS'
-
-    trace = StateMonitor(P, 'v', record=[1, 10, 100])
-
-    run(100*ms, report='text')
-
-    plt.plot(trace.t/ms, trace[1].v/mV)
-    plt.plot(trace.t/ms, trace[10].v/mV)
-    plt.plot(trace.t/ms, trace[100].v/mV)
-    plt.xlabel('t (ms)')
-    plt.ylabel('v (mV)')
-    plt.show()
-
-pars = ((0.02,    0.2,    -65,    6,    14), #tonic spiking
-        (0.02,    0.25,   -65,    6,    0.5), #phasic spiking
-        (0.02,    0.2,    -50,    2,    15), #tonic bursting
-        (0.02,    0.25,   -55,    0.05, 0.6), #phasic bursting
-        (0.02,    0.2,    -55,    4,    10), #mixed mode
-        (0.01,    0.2,    -65,    8,    30), #spike frequency adaptation
-        (0.02,    -0.1,   -55,    6,    0), #Class 1
-        (0.2,     0.26,   -65,    0,    0), #Class 2
-        (0.02,    0.2,    -65,    6,    7), #spike latency
-        (0.05,    0.26,   -60,    0,    0), #subthreshold oscillations
-        (0.1,     0.26,   -60,    -1,   0), #resonator
-        (0.02,    -0.1,   -55,    6,    0), #integrator
-        (0.03,    0.25,   -60,    4,    0), #rebound spike
-        (0.03,    0.25,   -52,    0,    0), #rebound burst
-        (0.03,    0.25,   -60,    4,    0), #threshold variability
-        (1,       1.5,    -60,    0,    -65), #bistability
-        (1,       0.2,    -60,    -21,  0), #DAP
-        (0.02,    1,      -55,    4,    0), #accomodation
-        (-0.02,   -1,     -60,    8,    80), #inhibition-induced spiking
-        (-0.026,  -1,     -45,    0,    80)) #inhibition-induced bursting
-
-def Izhikevich():
-    start_scope()
-
-    a, b, c, d, I = pars[15]
-
-    model = '''
-    dv/dt = (0.04*v*v + 5*v + 140 - u + I)/ms : 1 (unless refractory)
-    du/dt = (a*(b*v - u))/ms : 1 (unless refractory)
+    neuron_model = '''
+    a : 1
+    b : 1
+    c : 1
+    d : 1
+    I : 1
+    dv/dt = (0.04*v*v + 5*v + 140 - u + I)/ms : 1
+    du/dt = (a*(b*v - u))/ms : 1
     '''
-    threshold = 'v > 30' #threshold不能换行
-    reset = '''
+    neuron_threshold = 'v > 30' #threshold不能换行
+    neuron_reset = '''
     v = c
     u += d
     '''
-    #P = PoissonGroup(1, 0*Hz)
-    G = NeuronGroup(1, model, threshold=threshold, reset=reset)
-    G.v = -70
-    G.u = -20
+    
+    def __init__(self, num=1, *args, **kwargs):
+        super(Izhikevich, self).__init__(num, self.neuron_model, threshold=self.neuron_threshold, reset=self.neuron_reset, method='euler')
+        self.v = -70
+        self.u = -20
+        self.a, self.b, self.c, self.d, _ = self.params[0]
+        self.I = 0
 
-    #S = Synapses(P, G, on_pre='v += 10')
-    #S.connect(j='i')
+class STDP(Synapses):
+    __type__ = 'synapse'
 
-    state_monitor = StateMonitor(G, ['v', 'u'], record=True)
+    synapse_model = '''
+    w : 1
+    wmax : 1
+    taupre : second
+    taupost : second
+    apre : 1
+    apost : 1
+    ratepre : 1
+    ratepost : 1
+    dpre/dt = -pre/taupre : 1 (event-driven)
+    dpost/dt = -post/taupost : 1 (event-driven)
+    '''
+    synapse_pre='''
+    v_post += w
+    pre += apre
+    w = clip(w + ratepost*post, 0, wmax)
+    '''
+    synapse_post='''
+    post += apost
+    w = clip(w + ratepre*pre, 0, wmax)
+    '''
+    
+    cache = 'stdp.npy'
 
-    run(100*ms, report='text')
+    def __init__(self, source_neuron, target_neuron, *args, **kwargs):
+        super(STDP, self).__init__(source_neuron, target_neuron, self.synapse_model, on_pre=self.synapse_pre, on_post=self.synapse_post)
 
-    plt.plot(state_monitor.t/ms, state_monitor[0].v)
-    #plt.plot(state_monitor.t/ms, state_monitor[0].u)
-    plt.xlabel('t (ms)')
-    plt.ylabel('v (mV)')
-    plt.show()
+    def connect(self, *args, **kwargs):
+        super(STDP, self).connect(*args, **kwargs)
+        self.wmax = 1
+        self.taupre = self.taupost = 20*ms
+        self.apre = 0.01 * self.wmax
+        self.apost = -self.apre * self.taupre / self.taupost * 1.05
+        self.ratepre = 0.01
+        self.ratepost = 1
+
+        if os.path.isfile(self.cache):
+            stdp.w = np.load(self.cache)
+        else:
+            stdp.w = 'rand()*wmax'
+
+    def save(self):
+        np.save(self.cache, stdp.w)
+
+    def load(self):
+        stdp.w = np.load(self.cache)
+
+class Monitor(StateMonitor):
+    def __init__(self, node, record=True, *args, **kwargs):
+        self.type = 'other'
+        if hasattr(node, '__type__'):
+            if node.__type__ == 'neuron':
+                self.type = 'neuron'
+                super(Monitor, self).__init__(node, 'v', record=record, *args, **kwargs)
+            elif node.__type__ == 'synapse':
+                self.type = 'synapse'
+                super(Monitor, self).__init__(node, 'w', record=record, *args, **kwargs)
+        else:
+            super(Monitor, self).__init__(node, record=record, *args, **kwargs)
+    
+    def show(self, num=0):
+        if self.type == 'neuron':
+            plt.plot(self.t/ms, self.v[num], label='v%d' % num)
+            plt.legend()
+            plt.xlabel('t (ms)')
+            plt.ylabel('v (mV)')
+        elif self.type == 'synapse':
+            plt.plot(self.t/ms, self.w[num], label='w%d' % num)
+            plt.legend()
+            plt.xlabel('t (ms)')
+            plt.ylabel('v (mV)')
+
 
 if __name__ == '__main__':
-    Izhikevich()
+    start_scope()
+    poisson = Poisson(50, num=100)
+    izh = Izhikevich(100)
+    stdp = STDP(poisson, izh)
+    stdp.connect()
+
+    neuron_monitor = Monitor(izh)
+    synapse_monitor = Monitor(stdp)
+
+    run(10000*ms, report='text')
+    stdp.save()
+
+    plt.figure(figsize=(4, 8))
+    plt.subplot(211)
+    neuron_monitor.show()
+
+    plt.subplot(212)
+    synapse_monitor.show(0)
+    synapse_monitor.show(1)
+
+    plt.show()
