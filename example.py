63740c5e80de743798274f82f43b325034b8538b
zx013 <1453145267@qq.com>
2018-05-02 16:50:35 +0800

    step

--- example.py
+++ example.py
@@ -5,16 +5,62 @@ Created on Fri Apr 20 14:14:45 2018
 @author: zx013
 """
 import os
-import numpy as np
+#import numpy as np
+import brian2.numpy_ as np
 import matplotlib.pyplot as plt
 from brian2 import PoissonGroup, NeuronGroup, Synapses, StateMonitor
-from brian2 import Hz, ms, prefs, start_scope, run
+from brian2 import Hz, ms, mV, mA, nA, prefs, start_scope, run
 
 prefs.codegen.target = 'numpy'
 
 class Poisson(PoissonGroup):
     def __init__(self, h, num=1):
         super(Poisson, self).__init__(num, h*Hz)
+
+class HodgkinHuxley(NeuronGroup):
+    __type__ = 'neuron'
+    
+    neuron_model = '''
+    area = 20000*umetre**2 : metre**2
+    Cm = 1*ufarad*cm**-2*area : farad
+    gl = 5e-5*siemens*cm**-2*area : siemens
+    El = -65*mV : volt
+    EK = -90*mV : volt
+    ENa = 50*mV : volt
+    g_na = 100*msiemens*cm**-2*area : siemens
+    g_kd = 30*msiemens*cm**-2*area : siemens
+    VT = -63*mV : volt
+    dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
+    dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/(exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
+(exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
+    dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/(exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+    dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+    I : amp
+    '''
+    neuron_threshold = 'v > -40*mV'
+    neuron_reset = ''
+    
+    def __init__(self, num=1, *args, **kwargs):
+        super(HodgkinHuxley, self).__init__(num, self.neuron_model, threshold=self.neuron_threshold, reset=self.neuron_reset, method='exponential_euler')
+        self.v = -70*mV
+        self.I = 0.1*nA
+
+class LeakyIF(NeuronGroup):
+    __type__ = 'neuron'
+    
+    neuron_model = '''
+    tau = 10*ms : second
+    dv/dt = -v/tau + I*ohm/ms : volt (unless refractory)
+    I : amp
+    '''
+    neuron_threshold = 'v > 10*mV'
+    neuron_reset = 'v = -70*mV'
+    neuron_refractory = 10*ms
+    
+    def __init__(self, num=1, *args, **kwargs):
+        super(LeakyIF, self).__init__(num, self.neuron_model, threshold=self.neuron_threshold, reset=self.neuron_reset, refractory=self.neuron_refractory, method='exponential_euler')
+        self.v = -70*mV
+        self.I = 5*mA
 
 class Izhikevich(NeuronGroup):
     __type__ = 'neuron'
@@ -46,20 +92,20 @@ class Izhikevich(NeuronGroup):
     b : 1
     c : 1
     d : 1
-    I : 1
-    dv/dt = (0.04*v*v + 5*v + 140 - u + I)/ms : 1
-    du/dt = (a*(b*v - u))/ms : 1
+    dv/dt = (0.04*v*v/mV + 5*v + 140*mV - u + I/amp*mV)/ms : volt
+    du/dt = a*(b*v - u)/ms : volt
+    I : amp
     '''
-    neuron_threshold = 'v > 30' #threshold不能换行
+    neuron_threshold = 'v > 30*mV' #threshold不能换行
     neuron_reset = '''
-    v = c
-    u += d
+    v = c*mV
+    u += d*mV
     '''
     
     def __init__(self, num=1, *args, **kwargs):
         super(Izhikevich, self).__init__(num, self.neuron_model, threshold=self.neuron_threshold, reset=self.neuron_reset, method='euler')
-        self.v = -70
-        self.u = -20
+        self.v = -70*mV
+        self.u = -20*mV
         self.a, self.b, self.c, self.d, _ = self.params[0]
         self.I = 0
 
@@ -102,16 +148,16 @@ class STDP(Synapses):
         self.ratepre = 0.01
         self.ratepost = 1
 
-        if os.path.isfile(self.cache):
-            stdp.w = np.load(self.cache)
+        if False: #os.path.isfile(self.cache):
+            self.w = np.load(self.cache)
         else:
-            stdp.w = 'rand()*wmax'
+            self.w = '5' #'rand()*wmax'
 
     def save(self):
-        np.save(self.cache, stdp.w)
+        np.save(self.cache, self.w)
 
     def load(self):
-        stdp.w = np.load(self.cache)
+        self.w = np.load(self.cache)
 
 class Monitor(StateMonitor):
     def __init__(self, node, record=True, *args, **kwargs):
@@ -128,7 +174,7 @@ class Monitor(StateMonitor):
     
     def show(self, num=0):
         if self.type == 'neuron':
-            plt.plot(self.t/ms, self.v[num], label='v%d' % num)
+            plt.plot(self.t/ms, self.v[num]/mV, label='v')
             plt.legend()
             plt.xlabel('t (ms)')
             plt.ylabel('v (mV)')
@@ -141,23 +187,33 @@ class Monitor(StateMonitor):
 
 if __name__ == '__main__':
     start_scope()
-    poisson = Poisson(50, num=100)
-    izh = Izhikevich(100)
-    stdp = STDP(poisson, izh)
-    stdp.connect()
+    #poisson = Poisson(0)
+    hh = HodgkinHuxley()
+    lif = LeakyIF()
+    izh = Izhikevich()
+    #stdp = STDP(poisson, izh)
+    #stdp.connect()
 
-    neuron_monitor = Monitor(izh)
-    synapse_monitor = Monitor(stdp)
+    hhm = Monitor(hh)
+    lifm = Monitor(lif)
+    izhm = Monitor(izh)
+    #synapse_monitor = Monitor(stdp)
 
-    run(10000*ms, report='text')
-    stdp.save()
+    run(100*ms, report='text')
+    #stdp.save()
 
-    plt.figure(figsize=(4, 8))
-    plt.subplot(211)
-    neuron_monitor.show()
+    #plt.figure(figsize=(4, 8))
+    #plt.subplot(211)
+    #neuron_monitor.show()
 
-    plt.subplot(212)
-    synapse_monitor.show(0)
-    synapse_monitor.show(1)
+    #plt.subplot(212)
+    #synapse_monitor.show(0)
+    #synapse_monitor.show(1)
 
+    plt.plot(hhm.t/ms, hhm.v[0]/mV, label='HH')
+    #plt.plot(lifm.t/ms, lifm.v[0]/mV, label='LIF')
+    #plt.plot(izhm.t/ms, izhm.v[0]/mV, label='Izh')
+    plt.legend()
+    plt.xlabel('t (ms)')
+    plt.ylabel('v (mV)')
     plt.show()
