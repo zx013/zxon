0ff577a1e56c6410dd6e98c3234440f9c20ec7ff
zx013 <1453145267@qq.com>
2018-08-01 17:35:57 +0800

    depress not well

--- example.py
+++ example.py
@@ -9,6 +9,7 @@ import os
 import brian2.numpy_ as np
 import matplotlib.pyplot as plt
 from brian2 import *
+import random
 
 prefs.codegen.target = 'numpy'
 
@@ -55,16 +56,30 @@ class Izhikevich(NeuronGroup):
     dg/dt = -g / (5*ms) : amp
     dp : 1
     '''
+    neuron_input = '''
+    a : 1
+    b : 1
+    c : 1
+    d : 1
+    dv/dt = (0.04*v*v/mV + 5*v + 140*mV - u + I/amp*mV)/ms : volt (unless refractory)
+    du/dt = a*(b*v - u)/ms : volt (unless refractory)
+    I : amp
+    dp : 1
+    '''
     neuron_threshold = 'v > 30*mV' #threshold不能换行
     neuron_reset = '''
     v = c*mV
     u += d*mV
     '''
 
-    def __init__(self, num=1, spike_type='RS', *args, **kwargs):
-        super(Izhikevich, self).__init__(num, self.neuron_model, threshold=self.neuron_threshold, reset=self.neuron_reset, refractory=5*ms, method='euler')
+    def __init__(self, num=1, spike_type='RS', isinput=False, *args, **kwargs):
+        if isinput:
+            model = self.neuron_input
+        else:
+            model = self.neuron_model
+        super(Izhikevich, self).__init__(num, model, threshold=self.neuron_threshold, reset=self.neuron_reset, refractory=2*ms, method='euler')
         self.v = -70*mV
-        self.u = -20*mV
+        self.u = -14*mV
         self.dp = 1 #是否是抑制神经元，1/-1
         if spike_type == 'RS':
             spike_choice = 0
@@ -88,30 +103,33 @@ class STDP(Synapses):
     tpost : second
     type : 1
     depress : 1
+    speed = 0.5 : 1
     '''
-    #w += 1.11*exp(-((tpost - tpre)/ms + 2)**2/1.11**2) - 0.11
+    #1.11*exp(-((tpost - tpre)/ms + 2)**2/1.11**2) - 0.11
+    #1.21*exp(-((tpost - tpre)/ms + 12.7)**2/13.61**2) - 0.21
     synapse_pre='''
+    g_post += depress*w*amp
     tpre = t
     deltaw = type*(1.21*exp(-((tpost - tpre)/ms + 12.7)**2/13.61**2) - 0.21)
-    w = clip(w + deltaw, 0, 10)
-    g_post += 10*depress*w*amp
+    w = clip(w + speed*deltaw, 0, 10)
     type = 0
     '''
     synapse_post='''
     tpost = t
     deltaw = (1 - type)*(1.21*exp(-((tpost - tpre)/ms + 12.7)**2/13.61**2) - 0.21)
-    w = clip(w + deltaw, 0, 10)
-    g_post += 10*depress*w*amp
+    w = clip(w + speed*deltaw, 0, 10)
     type = 1
     '''
 
     def __init__(self, source_neuron, target_neuron, *args, **kwargs):
         super(STDP, self).__init__(source_neuron, target_neuron, self.synapse_model, on_pre=self.synapse_pre, on_post=self.synapse_post)
-        super(STDP, self).connect(*args, **kwargs)
+        super(STDP, self).connect(n=10, *args, **kwargs)
+        self.w = 5
         if hasattr(source_neuron, 'dp'):
-            self.depress = source_neuron.dp
+            self.depress = source_neuron.dp[0]
         else:
             self.depress = 1
+        self.delay = 'rand() * 15*ms'
 
 
 class Monitor(StateMonitor):
@@ -127,14 +145,15 @@ class Monitor(StateMonitor):
         else:
             super(Monitor, self).__init__(node, record=record, *args, **kwargs)
 
-    def show(self, num=0):
+    def show(self):
         if self.type == 'neuron':
-            plt.plot(self.t/ms, self.v[num]/mV, label='v%d' % num)
+            for i in range(len(self.v)):
+                plt.plot(self.t/ms, self.v[i]/mV, label='v%d' % i)
             plt.legend()
             plt.xlabel('t (ms)')
             plt.ylabel('v (mV)')
         elif self.type == 'synapse':
-            plt.plot(self.t/ms, self.w[num], label='w%d' % num)
+            plt.plot(self.t/ms, self.w[0], label='w')
             plt.legend()
             plt.xlabel('t (ms)')
             plt.ylabel('v (mV)')
@@ -142,86 +161,93 @@ class Monitor(StateMonitor):
 
 class SensingCircuit:
     def __init__(self, network):
-        self.CH_BL = Izhikevich(1, 'CH')
-        self.CH_GL = Izhikevich(1, 'CH')
-        self.CH_BR = Izhikevich(1, 'CH')
-        self.CH_GR = Izhikevich(1, 'CH')
-        network.add([self.CH_BL, self.CH_GL, self.CH_BR, self.CH_GR])
+        self.CH = Izhikevich(1, 'CH', True)
+        network.add(self.CH)
 
 class PunishmentCircuit:
     def __init__(self, network):
-        self.CH = Izhikevich(1, 'CH')
+        self.CH = Izhikevich(1, 'CH', True)
         network.add(self.CH)
 
 class ControlCircuit:
     def __init__(self, network):
-        self.RS_left = Izhikevich(1, 'RS')
-        self.RS_right = Izhikevich(1, 'RS')
-        network.add([self.RS_left, self.RS_right])
+        self.RS_L = Izhikevich(1, 'RS')
+        self.RS_R = Izhikevich(1, 'RS')
+        network.add([self.RS_L, self.RS_R])
 
 class DecisionCircuit:
     def __init__(self, network):
-        self.CH_left = Izhikevich(1, 'CH')
-        self.CH_right = Izhikevich(1, 'CH')
-        self.LTS_left = Izhikevich(1, 'LTS')
-        self.LTS_right = Izhikevich(1, 'LTS')
+        self.CH_L = Izhikevich(1, 'CH')
+        self.CH_R = Izhikevich(1, 'CH')
+        self.LTS_L = Izhikevich(1, 'LTS')
+        self.LTS_R = Izhikevich(1, 'LTS')
         self.RS = Izhikevich(1, 'RS')
         self.LTS = Izhikevich(1, 'LTS')
+        network.add([self.CH_L, self.CH_R, self.LTS_L, self.LTS_R, self.RS, self.LTS])
 
-        network.add([self.CH_left, self.CH_right, self.LTS_left, self.LTS_right, self.RS, self.LTS])
-
-        self.stdp1 = STDP(self.LTS, self.RS)
-        self.stdp2 = STDP(self.RS, self.CH_left)
-        self.stdp3 = STDP(self.RS, self.CH_right)
-        self.stdp4 = STDP(self.CH_left, self.LTS_right)
-        self.stdp5 = STDP(self.CH_right, self.LTS_left)
-        self.stdp6 = STDP(self.LTS_left, self.CH_left)
-        self.stdp7 = STDP(self.LTS_right, self.CH_right)
-        network.add([self.stdp1, self.stdp2, self.stdp3, self.stdp4, self.stdp5, self.stdp6, self.stdp7])
+        self.stdp1 = STDP(self.LTS, self.RS, j='i')
+        self.stdp2 = STDP(self.RS, self.CH_L, j='i')
+        self.stdp3 = STDP(self.RS, self.CH_R, j='i')
+        self.stdp4 = STDP(self.CH_L, self.LTS_L, condition='j!=i')
+        self.stdp5 = STDP(self.CH_L, self.LTS_R)
+        self.stdp6 = STDP(self.CH_R, self.LTS_R, condition='j!=i')
+        self.stdp7 = STDP(self.CH_R, self.LTS_L)
+        self.stdp8 = STDP(self.LTS_L, self.CH_L, j='i')
+        self.stdp9 = STDP(self.LTS_R, self.CH_R, j='i')
+        network.add([self.stdp1, self.stdp2, self.stdp3, self.stdp4, self.stdp5, self.stdp6, self.stdp7, self.stdp8, self.stdp9])
 
 
 class Fly:
     def __init__(self):
         pass
 
-
+defaultclock.dt = 500.*us
 if __name__ == '__main__':
     start_scope()
 
     network = Network()
-    poisson = Poisson(50)
-    network.add(poisson)
 
     sc = SensingCircuit(network)
     pc = PunishmentCircuit(network)
     cc = ControlCircuit(network)
     dc = DecisionCircuit(network)
 
-    stdp1 = STDP(poisson, sc.CH_BL)
-    stdp2 = STDP(sc.CH_BL, dc.RS)
-    stdp3 = STDP(pc.CH, dc.LTS)
-    stdp4 = STDP(pc.CH, dc.CH_left)
-    stdp5 = STDP(pc.CH, dc.CH_right)
-    stdp6 = STDP(dc.CH_left, cc.RS_left)
-    stdp7 = STDP(dc.CH_right, cc.RS_right)
-    network.add([stdp1, stdp2, stdp3, stdp4, stdp5, stdp6, stdp7])
+    stdp1 = STDP(sc.CH, dc.RS, j='i')
+    stdp2 = STDP(pc.CH, dc.LTS)
+    stdp3 = STDP(pc.CH, dc.CH_L)
+    stdp4 = STDP(pc.CH, dc.CH_R)
+    stdp5 = STDP(dc.CH_L, cc.RS_L)
+    stdp6 = STDP(dc.CH_R, cc.RS_R)
+    network.add([stdp1, stdp2, stdp3, stdp4, stdp5, stdp6])
 
-    #left = Monitor(cc.RS_left)
-    #right = Monitor(cc.RS_right)
-    n1 = Monitor(sc.CH_BL)
-    n2 = Monitor(dc.RS)
-    n3 = Monitor(dc.CH_left)
-    n4 = Monitor(dc.CH_right)
-    n5 = Monitor(dc.LTS_left)
-    n6 = Monitor(dc.LTS_right)
-    s1 = Monitor(dc.stdp6)
-    s2 = Monitor(dc.stdp7)
+    n1 = Monitor(dc.CH_L)
+    n2 = Monitor(dc.CH_R)
+    n3 = Monitor(dc.LTS_L)
+    n4 = Monitor(dc.LTS_R)
+    n5 = Monitor(dc.RS)
+    n6 = Monitor(pc.CH)
+    n7 = Monitor(dc.LTS)
+    #left = Monitor(cc.RS_L)
+    #right = Monitor(cc.RS_R)
 
-    n = [n1, n2, n3, n4, n5, n6, s1, s2]
+    s1 = Monitor(dc.stdp8)
+    s2 = Monitor(dc.stdp9)
+
+    n = [n1, n2, n3, n4, n5, n6, n7]
+    #n = [n6, n7, n5]
     network.add(n)
+
+    sc.CH.I[0] = 10*amp
+    pc.CH.I = 0*amp
     network.run(500*ms, report='text')
 
-    plt.figure(figsize=(4, 2 * len(n)))
+    pc.CH.I = 10*amp
+    network.run(500*ms, report='text')
+
+    pc.CH.I = 0*amp
+    network.run(500*ms, report='text')
+
+    plt.figure(figsize=(12, 2 * len(n)))
 
     for i in range(len(n)):
         plt.subplot(len(n) * 100 + 10 + i + 1)
