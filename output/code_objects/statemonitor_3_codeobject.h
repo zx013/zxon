aebf49cb136c022f8974add0cae7d3f88607a258
zx013 <1453145267@qq.com>
2018-09-06 16:01:13 +0800

    remove something

--- output/code_objects/statemonitor_3_codeobject.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INCLUDED_statemonitor_3_codeobject
-#define _INCLUDED_statemonitor_3_codeobject
-
-#include "objects.h"
-
-void _run_statemonitor_3_codeobject();
-
-
-#endif
--- output/code_objects/statemonitor_4_codeobject.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INCLUDED_statemonitor_4_codeobject
-#define _INCLUDED_statemonitor_4_codeobject
-
-#include "objects.h"
-
-void _run_statemonitor_4_codeobject();
-
-
-#endif
